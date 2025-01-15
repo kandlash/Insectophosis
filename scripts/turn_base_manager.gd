ed0ab0588f380f1297aac64c4794ee037e13a4a7
Konstantin Lashkulov <konstantin.laschkulov@yandex.com>
2025-01-15 23:40:24 +0300

    delay before end turn

--- scripts/turn_base_manager.gd
+++ scripts/turn_base_manager.gd
@@ -8,6 +8,7 @@ enum GameStates {
 var game_state = GameStates.player_turn
 @onready var battle_place := $"../battle_place"
 @onready var level_manager := $"../LevelManager"
+@onready var timer: Timer = $Timer
 
 signal end_turned
 
@@ -21,10 +22,14 @@ func end_turn(by_who):
 		
 	await battle_place.end_turn
 	if by_who == "enemy":
-		level_manager.start_s_cam_transition()
+		timer.start()
 		await level_manager.end_trans
 	game_state = GameStates.enemy_turn if by_who == "player" else GameStates.player_turn
 	if by_who == "enemy":
 		Globals.player.reset()
 		Globals.game_ui.update_recources()
 	end_turned.emit(by_who)
+
+
+func _on_timer_timeout() -> void:
+	level_manager.start_s_cam_transition()
